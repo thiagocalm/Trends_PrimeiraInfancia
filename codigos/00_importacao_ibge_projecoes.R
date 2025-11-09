0bd6a0f14d37167783ba5ff26ecde90a74fa5a09
thiagocalm <thiagocalm@outlook.com>
2025-11-09 22:46:26 +0100

    importacao - ibge, projecoes, uf - tratamento e exportacao

--- codigos/00_importacao_ibge_projecoes.R
+++ codigos/00_importacao_ibge_projecoes.R
@@ -24,6 +24,13 @@ p_load(tidyverse, arrow, readxl)
 dir_input = file.path("dados","ibge","projecoes","raw")
 dir_output = file.path("dados","ibge","projecoes")
 
+# codigos de selecao
+codigos <- c(0,2,22)
+
+# anos de selecao
+presente <- c(2000:2024)
+futuro <- c(2025:2040)
+
 # Importacao ---------------------------------------------------
 
 # importacao
@@ -35,3 +42,57 @@ df <- read_xlsx(
 
 # Tratamentos -------------------------------------------------------------
 
+# transformacao em formato longo
+
+df <- df |>
+  pivot_longer(
+    `2000`:`2070`,
+    names_to = "ano",
+    values_to = "populacao"
+  )
+
+# ajuste de nomes das colunas
+
+nomes <- c("idade","sexo","cod","sigla","local","ano","populacao")
+
+colnames(df) <- nomes
+
+# ajuste de classe das variaveis
+
+df <- df |>
+  mutate(
+    ano = as.numeric(ano)
+  )
+
+
+# Selecao dos codigos de interesse ----------------------------------------
+
+df <- df |>
+  filter(cod %in% codigos)
+
+# Divisao em duas bases de dados ------------------------------------------
+
+# estimativas
+
+df_estimativas <- df |>
+  filter(ano %in% presente)
+
+# projecoes
+
+df_projecoes <- df |>
+  filter(ano %in% futuro)
+
+
+# Exportacao dos dados ----------------------------------------------------
+
+# estimativas
+write_parquet(
+  df_estimativas,
+  file.path(dir_output,"ibge_proj_uf_estimativas.parquet")
+)
+
+# estimativas
+write_parquet(
+  df_projecoes,
+  file.path(dir_output,"ibge_proj_uf_projecoes.parquet")
+)
