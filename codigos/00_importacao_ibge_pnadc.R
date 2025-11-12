f8920975f223cb61ee5d9bd9ac28d7f23915679a
thiagocalm <thiagocalm@outlook.com>
2025-11-12 22:25:36 +0100

    importacao - pnadc

--- codigos/00_importacao_ibge_pnadc.R
+++ codigos/00_importacao_ibge_pnadc.R
@@ -59,7 +59,8 @@ pnadc <- pnadc |>
     ind_sexo = v2007
   ) |>
   mutate(
-    ind_dom = paste0()
+    ind_dom = as.numeric(paste0(upa, v1008, v1014)),
+    ind_pes = as.numeric(paste0(ind_dom,v2003)),
     ind_priminfancia = case_when(idade %in% 0:5 ~ 1, TRUE ~ 0),
     ind_priminfancia_cat = case_when(
       idade %in% 0:3 ~ 1,
@@ -68,6 +69,7 @@ pnadc <- pnadc |>
     ),
     ind_brasil = 1,
     ind_regiao_NE = (uf %in% 20:29),
+    ind_uf_PI = (uf == 22),
     ind_raca = case_when(
       v2010 %in% c(2, 4) ~ 1,
       v2010 %in% c(5) ~ 2,
@@ -77,11 +79,34 @@ pnadc <- pnadc |>
     renda = vd4019,
 
     # falta escolaridade
-
     frequenta_creche = case_when(v3003a %in% 1:2 ~ 1, TRUE ~ 0),
     frequenta_preesc = case_when(v3003a >= 2 ~ 1, TRUE ~ 0),
+    ind_esc_freq = case_when(frequenta_creche == 1 | frequenta_preesc == 1 ~ 1, TRUE ~ 0),
     across(where(is.logical), as.numeric)
   )
 
+# criando variavel de renda per capita
+
+pnadc <- pnadc |>
+  mutate(renda = case_when(is.na(renda) ~ 0, TRUE ~ renda)) |>
+  mutate(
+    pessoas = sum(ind_brasil),
+    renda = sum(renda),
+    .by = ind_dom
+  ) |>
+  # renda domiciliar per capita
+  mutate(
+    renda_pc = renda/pessoas
+  )
+
+# criando variaveis de renda ...
+
 # selecionando variaveis
 
+pnadc <- pnadc |>
+  select(
+    ano, uf, peso, ind_dom, ind_pes, idade, ind_situacao, ind_sexo, ind_priminfancia, ind_priminfancia_cat,
+    ind_brasil, ind_regiao_NE, ind_uf_PI, ind_raca,
+    #ind_renda,
+    ind_esc_freq, frequenta_creche, frequenta_preesc
+  )
